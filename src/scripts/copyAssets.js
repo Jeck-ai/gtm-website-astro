cb85afbc5db166df5acf4b6bf3b42f507db9a40e
Robert Douglass <rob@robshouse.net>
2025-03-30 20:01:42 +0200

    Update the copying script

--- src/scripts/copyAssets.js
+++ src/scripts/copyAssets.js
@@ -1,5 +1,10 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get the current file's directory in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Define paths
 const srcDir = path.join(__dirname, '../../src/assets/blog');
